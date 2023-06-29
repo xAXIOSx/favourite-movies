5b90f0e8b2db85f23070f141f3789270e748171a
Axios <diman0719192323@gmail.com>
2023-06-29 10:33:14 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <main>
+    <header class="header">
+      <img src="../public/logo.svg" alt="logo" class="header-logo" />
+      <h2>My favourite movies</h2>
+    </header>
+    <div class="tabs">
+      <button :class="['btn', {'btn_green':movieStore.activeTab === 1}]">Favourite</button>
+      <button :class="['btn', {'btn_green':movieStore.activeTab === 2}]">Search</button>
+    </div>
+    <div class="movies" v-if="movieStore.activeTab === 1">
+      <h3>All Movies:</h3>
+      <Movie
+        v-for="movie in movieStore.movies"
+        :movie="movie"
+        :key="movie.id"
+      />
+    </div>
+    <div class="search" v-else>
+      Search
+    </div>
+  </main>
+</template>
+
+<script setup>
+import Movie from "./components/Movie.vue";
+import { useMovieStore } from "./stores/movieStore";
+const movieStore = useMovieStore();
+
+console.log(movieStore);
+</script>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  &-logo {
+    max-width: 50px;
+    margin-right: 10px;
+  }
+}
+
+.tabs {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+}
+</style>
---
+++ src/components/Movie.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="movie">
+    <img
+      :src="`https://image.tmdb.org/t/p/w300_and_h450_bestv2${movie.poster_path}`"
+      :alt="movie.original_title"
+      class="movie-img"
+    />
+    <div>
+      <div class="movie-name">
+        {{ movie.original_title }} ({{ movie.release_date }})
+      </div>
+      <span class="movie-overview">{{ movie.overview }}</span>
+      <div class="movie-buttons">
+        <button class="btn movie-buttons-watched">
+          <span v-if="!movie.isWatched">Watched</span>
+          <span v-else>Unwatched</span>
+        </button>
+        <button class="btn movie-buttons-delete">Delete</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  movie: {
+    type: Object,
+    default: () => {},
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.movie {
+  display: grid;
+  grid-template-columns: 200px 1fr;
+  column-gap: 30px;
+  margin-bottom: 20px;
+  border: 2px solid #efefef;
+  padding: 10px;
+
+  &-accept {
+    margin-right: 10px;
+  }
+
+  &-img {
+    width: 200px;
+    height: 200px;
+    object-fit: cover;
+    border-radius: 50%;
+  }
+
+  &-name {
+    display: flex;
+    align-items: center;
+    font-size: 20px;
+    margin-bottom: 20px;
+  }
+
+  &-overview {
+    display: block;
+    margin-bottom: 20px;
+  }
+
+  &-buttons {
+    display: flex;
+    gap: 10px;
+
+    &-watched {
+      color: #fff;
+      background: #1eb4c3;
+      &__icon {
+        width: 15px;
+        margin-left: 10px;
+      }
+    }
+    &-delete {
+      color: #fff;
+      background: #ff2a2a;
+    }
+  }
+}
+</style>
