0a8af422af0c5c5e4e2c21075de7a836109d1d5d
Axios <diman0719192323@gmail.com>
2023-07-11 12:37:25 +0500

    composition API

--- src/stores/movieStore.ts
+++ src/stores/movieStore.ts
@@ -1,108 +1,58 @@
 import { defineStore } from "pinia";
-import movieObj from '../types/MovieType'
+import movieObj from "../types/MovieType";
 import { computed, ref } from "vue";
 
-interface State {
-  movies: movieObj[],
-  activeTab: number
-}
-type GetMovie = (id:number) => movieObj
-
-// export const useMovieStore = defineStore("movieStore", {
-//   state: ():State => ({
-//     movies: [],
-//     activeTab: 2
-//   }),
-//   getters: {
-//     favouriteMovies():movieObj[]{
-//       return this.movies.filter((movie)=>{
-//         return movie.isFavourite
-//       })
-//     },
-//     moviesCounts():{[key: string]:number}{
-//       return {
-//         'all': this.movies.length,
-//         'favourite': this.favouriteMovies.length
-//       }
-//     },
-//     getMovie(state):GetMovie {
-//       return function(id:number):movieObj{
-//         let movieIdx = state.movies.findIndex(movie => movie.id == id)
-//         return state.movies[movieIdx]
-//       }
-//     },
-//   },
-//   actions: {
-//     favouriteToggle(id: number){
-//       let movie = this.getMovie(id)
-//       let isFavourite = movie.isFavourite
-//       console.log(movie);
-//       movie.isFavourite = !isFavourite
-//     },
-//     tabToggle(tab: number){
-//       this.activeTab = tab
-//     },
-//     addMovie(movie:movieObj){
-//       this.movies.push(movie)
-//     },
-//     deleteMovie(id: number){
-//       let movieIdx = this.movies.findIndex(movie => movie.id == id)
-//       this.movies.splice(movieIdx,1)
-//       // let movieIdx = this.movies.findIndex((movie) => movie.id == id)
-//     }
-//   }
-// });
 
 export const useMovieStore = defineStore("movieStore", () => {
-  let movies = ref([])
-  let activeTab = ref(2)
+  let movies = ref<movieObj[]>([]);
+  let activeTab = ref(2);
 
-  const favouriteMovies:movieObj[] = computed(()=>{
-    return movies.value.filter((movie:movieObj)=>{
-      return movie.isFavourite
-    })
-  })
+  const favouriteMovies = computed((): movieObj[] => {
+    return movies.value.filter((movie: movieObj) => {
+      return movie.isFavourite;
+    });
+  });
 
-  const moviesCounts = computed(()=>{():{[key: string]:number} => {
-          return {
-            'all': movies.value.length,
-            'favourite': favouriteMovies.length
-          }
-        }})
-        const getMovie = (state):GetMovie => {
-          return function(id:number):movieObj{
-            let movieIdx = state.movies.findIndex(movie => movie.id == id)
-            return state.movies[movieIdx]
-          }
-        }
-  
-  const favouriteToggle = (id: number) => {
-      let movie = getMovie(id)
-      let isFavourite = movie.isFavourite
-      console.log(movie);
-      movie.isFavourite = !isFavourite
-  }
-    const tabToggle = (tab: number) => {
-      activeTab.value = tab
-    }
-    const addMovie = (movie:movieObj) => {
-      movies.value.push(movie)
-    }
-    const deleteMovie = (id: number) => {
-      let movieIdx = movies.value.findIndex(movie => movie.id == id)
-      movies.value.splice(movieIdx,1)
-    }
+  const moviesCounts = computed((): { [key: string]: number } => {
     return {
-      movies,
-      activeTab,
-      favouriteMovies,
-      moviesCounts,
-      getMovie,
-      favouriteToggle,
-      tabToggle,
-      addMovie,
-      deleteMovie,
-    }
-  }
+      all: movies.value.length,
+      favourite: favouriteMovies.value.length,
+    };
+  });
 
+  const getMovie = (id: number) => {
+      let movieIdx = movies.value.findIndex((movie) => movie.id == id);
+      return movies.value[movieIdx];
+  };
+
+  const favouriteToggle = (id: number) => {
+    let movie:movieObj = getMovie(id);
+    let isFavourite = movie.isFavourite;
+    movie.isFavourite = !isFavourite;
+  };
+
+  const tabToggle = (tab: number) => {
+    activeTab.value = tab;
+  };
+
+  const addMovie = (movie: movieObj) => {
+    movies.value.push(movie);
+  };
+
+  const deleteMovie = (id: number) => {
+    let movieIdx = movies.value.findIndex((movie) => movie.id == id);
+    movies.value.splice(movieIdx, 1);
+  };
+
+  return {
+    movies,
+    activeTab,
+    favouriteMovies,
+    moviesCounts,
+    getMovie,
+    favouriteToggle,
+    tabToggle,
+    addMovie,
+    deleteMovie,
+  };
 });
--- src/stores/searchStore.ts
+++ src/stores/searchStore.ts
@@ -2,31 +2,37 @@ import { defineStore } from "pinia";
 import axios from "axios";
 import { useMovieStore } from "./movieStore";
 import movieObj from "../types/MovieType";
+import { ref } from "vue";
 
-const url =
-  "https:api.themoviedb.org/3/search/movie?api_key=d6dbc1c44a0c08f5c7c468222a0a168c&query=";
 
-export const useSearchStore = defineStore("searchStore", {
-  state: () => ({
-    movies: [],
-    isLoading: false,
-  }),
-  actions: {
-    getMovies(search: string) {
-      this.isLoading = true;
-      setTimeout(() => {
-        axios.get(`${url}${search}`).then((res) => {
-          this.movies = res.data.results;
-          this.isLoading = false;
-        });
-      }, 1000);
-    },
-    addMovieToWatched(id: number) {
-      const movieStore = useMovieStore();
-      let movieIdx = this.movies.findIndex((movie) => movie["id"] == id);
-      let movie: movieObj = this.movies[movieIdx];
 
-      movieStore.addMovie({ ...movie, isFavourite: false });
-    },
-  },
+export const useSearchStore = defineStore("searchStore", () => {
+  const movies = ref<movieObj[]>([]);
+  const isLoading = ref(false);
+  const url = "https:api.themoviedb.org/3/search/movie?api_key=d6dbc1c44a0c08f5c7c468222a0a168c&query=";
+
+  const getMovies = (search: string) => {
+    isLoading.value = true;
+    setTimeout(() => {
+      axios.get(`${url}${search}`).then((res) => {
+        movies.value = res.data.results;
+        isLoading.value = false;
+      });
+    }, 1000);
+  };
+
+  const addMovieToWatched = (id: number) => {
+    const movieStore = useMovieStore();
+    let movieIdx = movies.value.findIndex((movie) => movie["id"] == id);
+    let movie: movieObj = movies.value[movieIdx];
+
+    movieStore.addMovie({ ...movie, isFavourite: false });
+  };
+
+  return {
+    movies,
+    isLoading,
+    getMovies,
+    addMovieToWatched,
+  };
 });
